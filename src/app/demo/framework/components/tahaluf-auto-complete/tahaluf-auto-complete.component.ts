417881737823859cac0459f598a75e7ed61bdea0
Tarek Akel <thetarekakel@gmail.com>
2021-12-05 10:59:49 +0400

    framework-componet-test

--- src/app/demo/framework/components/tahaluf-auto-complete/tahaluf-auto-complete.component.ts
+++ src/app/demo/framework/components/tahaluf-auto-complete/tahaluf-auto-complete.component.ts
@@ -1,29 +1,34 @@
-import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+} from '@angular/core';
 import { BaseComponent } from '../../core/base-component';
 import { PrimengGridHelper } from './../../helpers/primeng-grid-helper';
-import { SearchCriteria } from './../../view-models/grid/search-criteria';
-import { PagedResult } from './../../view-models/grid/paged-result';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CustomerProfileSearchFilter } from '../../../shared/view-models/customer-profile-search-filter';
 import { CustomerProfileService } from '../../../shared/service/CustomerProfileService';
 
-const noop = () => {
-};
+const noop = () => {};
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
   useExisting: forwardRef(() => TahalufAutoCompleteComponent),
-  multi: true
+  multi: true,
 };
 
 @Component({
   selector: 'tahaluf-auto-complete',
   templateUrl: './tahaluf-auto-complete.component.html',
   styleUrls: ['./tahaluf-auto-complete.component.css'],
-  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR],
 })
-export class TahalufAutoCompleteComponent extends BaseComponent implements ControlValueAccessor {
-
+export class TahalufAutoCompleteComponent
+  extends BaseComponent
+  implements ControlValueAccessor
+{
   //The internal data model
   private innerValue: any = '';
 
@@ -35,7 +40,7 @@ export class TahalufAutoCompleteComponent extends BaseComponent implements Contr
   //get accessor
   get value(): any {
     return this.innerValue;
-  };
+  }
 
   //set accessor including call the onchange callback
   set value(v: any) {
@@ -52,22 +57,18 @@ export class TahalufAutoCompleteComponent extends BaseComponent implements Contr
 
   //From ControlValueAccessor interface
   writeValue(value: any) {
-
     if (value !== this.innerValue) {
-
       this.innerValue = value;
     }
   }
 
   //From ControlValueAccessor interface
   registerOnChange(fn: any) {
-
     this.onChangeCallback = fn;
   }
 
   //From ControlValueAccessor interface
   registerOnTouched(fn: any) {
-
     this.onTouchedCallback = fn;
   }
 
@@ -79,17 +80,22 @@ export class TahalufAutoCompleteComponent extends BaseComponent implements Contr
 
   // #region Inputs
 
-  @Input('items') items: any[];
-  @Input('value-field') valueField: string;
-  @Input('text-field') textField: string;
+  @Input('items') items: any[] = [];
+  @Input('value-field') valueField: string | undefined;
+  @Input('text-field') textField: string | undefined;
   @Input('force-selection') forceSelection: boolean = true;
 
   //Optional Inputs
 
-  @Input('dropdown-mode') dropdownMode: string = 'blank';//set any value except 'blank' to prevent fetch data when click on down arrow
+  @Input('dropdown-mode') dropdownMode: string = 'blank'; //set any value except 'blank' to prevent fetch data when click on down arrow
   @Input('multiple-selected') multipleSelected: boolean = false;
-  @Input('placeholder') placeholder: string = this.translationService.translate('Common.AutoCompletePlaceholder');
-  @Input('empty-message') emptyMessage: string = this.translationService.translate('Common.AutoCompleteEmptyMessage');
+  @Input('placeholder') placeholder: string = '';
+  //  this.translationService.translate(
+  //   'Common.AutoCompletePlaceholder'
+  // );
+  @Input('empty-message') emptyMessage: string = '';
+  // =
+  //   this.translationService.translate('Common.AutoCompleteEmptyMessage');
 
   @Output('onSelect') autoCompleteSelectedChanged = new EventEmitter<any>();
 
@@ -97,18 +103,18 @@ export class TahalufAutoCompleteComponent extends BaseComponent implements Contr
 
   // #region Constructor
 
-  constructor(private primengGridHelper: PrimengGridHelper<CustomerProfileSearchFilter>,
-    private customerProfileService: CustomerProfileService) {
-
+  constructor(
+    private primengGridHelper: PrimengGridHelper<CustomerProfileSearchFilter>,
+    private customerProfileService: CustomerProfileService
+  ) {
     super();
   }
 
-  // #endregion 
+  // #endregion
 
   // #region Methods
 
-  searchItems(event) {
-
+  searchItems(event: any) {
     this.searchItemsMethod.emit(event);
   }
 
@@ -116,5 +122,5 @@ export class TahalufAutoCompleteComponent extends BaseComponent implements Contr
     console.log(this.value);
     this.autoCompleteSelectedChanged.emit(this.value);
   }
-  // #endregion  
+  // #endregion
 }
--- src/app/demo/framework/components/tahaluf-date-time-picker/tahaluf-date-time-picker.component.ts
+++ src/app/demo/framework/components/tahaluf-date-time-picker/tahaluf-date-time-picker.component.ts
@@ -1,28 +1,36 @@
-﻿import { Component, Input, Output, EventEmitter, forwardRef, OnInit } from '@angular/core';
+﻿import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+  OnInit,
+} from '@angular/core';
 import { BaseComponent } from '../../core/base-component';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Data } from '@angular/router';
 
-const noop = () => {
-};
+const noop = () => {};
 
 export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
   useExisting: forwardRef(() => TahalufDateTimePickerComponent),
-  multi: true
+  multi: true,
 };
 
 @Component({
   selector: 'tahaluf-date-time-picker',
   templateUrl: './tahaluf-date-time-picker.component.html',
   styleUrls: ['./tahaluf-date-time-picker.component.css'],
-  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR],
 })
-export class TahalufDateTimePickerComponent extends BaseComponent implements ControlValueAccessor, OnInit {
-
+export class TahalufDateTimePickerComponent
+  extends BaseComponent
+  implements ControlValueAccessor, OnInit
+{
   //The internal data model
   private innerValue: any = '';
-  event: string;
+  event: string | undefined;
 
   //Placeholders for the callbacks which are later providesd
   //by the Control Value Accessor
@@ -32,11 +40,10 @@ export class TahalufDateTimePickerComponent extends BaseComponent implements Con
   //get accessor
   get value(): any {
     return this.innerValue;
-  };
+  }
 
   //set accessor including call the onchange callback
   set value(v: any) {
-    
     if (v !== this.innerValue) {
       this.innerValue = v;
       this.onChangeCallback(v);
@@ -50,22 +57,18 @@ export class TahalufDateTimePickerComponent extends BaseComponent implements Con
 
   //From ControlValueAccessor interface
   writeValue(value: any) {
-    
     if (value !== this.innerValue) {
-   
       this.innerValue = value;
     }
   }
 
   //From ControlValueAccessor interface
   registerOnChange(fn: any) {
-
     this.onChangeCallback = fn;
   }
 
   //From ControlValueAccessor interface
   registerOnTouched(fn: any) {
-
     this.onTouchedCallback = fn;
   }
 
@@ -75,18 +78,16 @@ export class TahalufDateTimePickerComponent extends BaseComponent implements Con
   }
   // #endregion
 
-  ngOnInit() {
-
+  override ngOnInit(): void {
     if (this.placeholder == undefined) {
-
       this.placeholder = this.dateformat;
     }
   }
 
   // #region Optional Inputs
   @Input('disabled') disabled: boolean = false;
-  @Input('disabled-dates') disabledDates: Array<Date> = null;
-  @Input('disabled-days') disabledDays: Array<number> = null;
+  @Input('disabled-dates') disabledDates: Array<Date> = new Array<Date>();
+  @Input('disabled-days') disabledDays: Array<number> = new Array<number>();
   @Input('show-button-bar') showButtonBar: boolean = true;
   @Input('readonly-input') readonlyInput: boolean = false;
   @Input('show-time') showTime: boolean = true;
@@ -96,10 +97,10 @@ export class TahalufDateTimePickerComponent extends BaseComponent implements Con
   @Input('year-navigator') yearNavigator: boolean = true;
   @Input('year-range') yearRange: string = this.dateYearRange;
   @Input('date-format') dateformat: string = this.dateFormat;
-  @Input('placeholder') placeholder: string;
-  @Input('min-date') minDate: Date = null;
-  @Input('max-date') maxDate: Date = null;
-  @Input('selection-mode') selectionMode: string = "single";
+  @Input('placeholder') placeholder: string | undefined;
+  @Input('min-date') minDate: Date | undefined;
+  @Input('max-date') maxDate: Date | undefined;
+  @Input('selection-mode') selectionMode: string = 'single';
   @Input('required') required: boolean = false;
   // #endregion
 
@@ -109,10 +110,9 @@ export class TahalufDateTimePickerComponent extends BaseComponent implements Con
 
   // #region Event
 
-  onChangeEvent(event) {
+  onChangeEvent(event: any) {
     this.onChange.emit(event);
   }
 
-  // #endregion  
-
+  // #endregion
 }
